f70af168b17b761891498c35a5a2fb3ccc82f65d
roycedinho <roycedinho@gmail.com>
2025-07-20 17:26:11 +0100

    committed

---
+++ Lesson 4/main.py
@@ -0,0 +1,5 @@
+num = 23
+print("Table of 23")
+for i in range(1,11):
+    mul = num*1
+    print("23 x %d = %d" (i, mul))
---
+++ Lesson 4/main3.py
@@ -0,0 +1,7 @@
+num = 1
+sum = 0
+while(num<=10):
+    sum = sum+num
+    num = num+1
+    
+print("The sum of First 10 Natural Numbers :" ,sum)
