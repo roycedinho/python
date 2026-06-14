2ec8230e0553b73e8a55cdab58fb79174618fea0
roycedinho <roycedinho@gmail.com>
2026-06-14 17:46:54 +0100

    commit

---
+++ SQLITE/connect.py
@@ -0,0 +1,31 @@
+#### **1. Import Database**
+"""
+
+# Import file from your system
+from google.colab import files
+file = files.upload()
+
+"""#### **2. Connect with SQLite Database**"""
+
+# Connect with sqlite database
+# Import necessary libraries
+import sqlite3
+
+database = 'database.sqlite'
+
+# Connect to the database (Only needed once)
+conn = sqlite3.connect(database)
+print('Opened data successfully')
+
+# Read SQL query for getting all the tables of database into a dataframe
+# Here SELECT * means select all
+import pandas as pd
+
+# Clean multi-line query string
+query = """
+SELECT * FROM sqlite_master 
+WHERE type='table';
+"""
+
+tables = pd.read_sql(query, conn)
+tables
